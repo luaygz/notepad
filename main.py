504d882eef138058aa11ac698fda7e53f287e8a7
Luay Gharzeddine <luaygharzeddine@gmail.com>
2022-11-02 22:06:01 +0200

    commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import Text, Menu
 from tkinter.ttk import Label
+from tkinter import Tk
+from ttkthemes import ThemedTk
+from tkinter.font import Font
 
 from utils import do_popup, close_popup, select_all, delete_selection, paste
 
@@ -23,15 +26,39 @@ class ScrollableText(Text):
         self.x_scroll_bar.config(command=self.xview)
         self.y_scroll_bar.config(command=self.yview)
 
+        self.tag_configure("BOLD", font=Font(family="Helvetica", size=14, weight="bold"))
+
+    def highlight_pattern(self, pattern, tag, start="1.0", end="end",
+                          regexp=False):
+        '''Apply the given tag to all text that matches the given pattern
+
+        If 'regexp' is set to True, pattern will be treated as a regular
+        expression according to Tcl's regular expression syntax.
+        '''
+
+        start = self.index(start)
+        end = self.index(end)
+        self.mark_set("matchStart", start)
+        self.mark_set("matchEnd", start)
+        self.mark_set("searchLimit", end)
+
+        count = tk.IntVar()
+        while True:
+            index = self.search(pattern, "matchEnd", "searchLimit",
+                                count=count, regexp=regexp)
+            if index == "": break
+            if count.get() == 0: break # degenerate pattern which matches zero-length strings
+            self.mark_set("matchStart", index)
+            self.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
+            self.tag_add(tag, "matchStart", "matchEnd")
+
 class StatusBar(Label):
     def __init__(self, master=None, **kwargs):
         self.word_count = tk.StringVar()
         self.word_count.set("")
         Label.__init__(self, master, textvariable=self.word_count, **kwargs)
-        self.update("")
 
-    def update(self, text):
-        wc = len(text.split())
+    def update(self, wc):
         self.word_count.set(f"Word Count -> {wc}")
 
 class PopupMenu(Menu):
@@ -43,40 +70,83 @@ class PopupMenu(Menu):
         self.add_command(label="Paste", command=lambda: paste(master))
         self.add_command(label="Save")
 
+class Notepad(Tk):
+    def __init__(self, **kwargs):
+        Tk.__init__(self, **kwargs)
+        
+        self.title("Notepad")
+        
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        self.geometry(f"{screen_width}x{screen_height}")
+        
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-window = tk.Tk()
-# style = ttk.Style()
-window.title("Notepad")
-window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}")
-window.grid_columnconfigure(1, weight=1)
-window.grid_rowconfigure(0, weight=1)
+        self.menuBar = tk.Menu(self)
+        self.fileMenu = tk.Menu(self.menuBar, tearoff=0)
+        self.editMenu = tk.Menu(self.menuBar, tearoff=0)
+        self.viewMenu = tk.Menu(self.menuBar, tearoff=0)
+        self.helpMenu = tk.Menu(self.menuBar, tearoff=0)
+        self.config(menu=self.menuBar)
 
-menuBar = tk.Menu(window)
-window.config(menu=menuBar)
-fileMenu = tk.Menu(menuBar, tearoff=0)
-editMenu = tk.Menu(menuBar, tearoff=0)
-viewMenu = tk.Menu(menuBar, tearoff=0)
-helpMenu = tk.Menu(menuBar, tearoff=0)
+        # Main text area
+        self.text = ScrollableText(self, undo=True, autoseparators=True, maxundo=-1)
+        self.text.frame.grid(row=0, column=1, sticky="NSEW")
 
-text_area = ScrollableText(window, undo=True, autoseparators=True, maxundo=-1)
-text_area.frame.grid(row=0, column=1, sticky="NSEW")
-text_area.focus()
+        # Line numbers
+        self.line_numbers = tk.Canvas(self, width="30", height="500")
+        self.line_numbers.grid(row=0, column=0, sticky='NS', pady=1, rowspan=3)
 
-line_numbers = tk.Canvas(window, width="30", height="500")
-line_numbers.grid(row=0, column=0, sticky='NS', pady=1, rowspan=3)
+        # Status bar on the bottom
+        self.status_bar = StatusBar(self)
+        self.status_bar.grid(row=2, column=1, columnspan=2, sticky="EW")
 
-status_bar = StatusBar(window)
-status_bar.grid(row=2, column=1, columnspan=2, sticky="EW")
+        # Right-click popup menu
+        self.popup_menu = PopupMenu(self.text)
 
-text_area.bind("<space>", lambda event: text_area.edit_separator())
-text_area.bind("<Return>", lambda event: text_area.edit_separator())
-text_area.bind("<KeyRelease>", lambda event: status_bar.update(text_area.get('1.0', tk.END)))
-text_area.bind("<<Paste>>", lambda event: delete_selection(text_area))
-text_area.bind("<Control-Key-a>", lambda event: select_all(text_area))
-text_area.bind("<Control-Key-A>", lambda event: select_all(text_area))
+        # Custom undo/redo behavior, undo deletes until previous word
+        self.text.bind("<space>", lambda event: self.text.edit_separator())
+        self.text.bind("<Return>", lambda event: self.text.edit_separator())
 
-popup_menu = PopupMenu(text_area)
-text_area.bind("<Button-3>", lambda event: do_popup(event.x_root, event.y_root, popup_menu))
-popup_menu.bind("<FocusOut>", lambda event: close_popup(popup_menu))
+        # Update word count
+        self.text.bind("<KeyRelease>", self.redraw)
 
-window.mainloop()
+        # By default ctrl+v doesn't overwrite selected text
+        self.text.bind("<<Paste>>", lambda event: delete_selection(self.text))
+
+        # Select all
+        self.text.bind("<Control-Key-a>", lambda event: select_all(self.text))
+        self.text.bind("<Control-Key-A>", lambda event: select_all(self.text)) # In case caps lock
+
+        # Create and close right click popup
+        self.text.bind("<Button-3>", lambda event: do_popup(event.x_root, event.y_root, popup_menu)) # Right click
+        self.popup_menu.bind("<FocusOut>", lambda event: close_popup(popup_menu))
+
+        self.text.focus()
+        self.redraw()
+
+    def redraw(self, event=None):
+        # Update status bar
+        words = self.text.get('1.0', tk.END).split()
+        word_count = len(words)
+        self.status_bar.update(word_count)
+
+        # Draw line numbers
+        self.line_numbers.delete("all")
+        index = "1.0"
+        while True:
+            line_info = self.text.dlineinfo(index)
+            if line_info is None:
+                break
+            x_coord = 2
+            y_coord = line_info[1]
+            line_num = str(index).split(".")[0]
+            self.line_numbers.create_text(x_coord, y_coord, anchor="nw", text=line_num, fill="black")
+            index = self.text.index(f"{index}+1line")
+
+        # Highlight
+        # self.text.highlight_pattern("a", "BOLD")
+
+notepad = Notepad()
+notepad.mainloop()
