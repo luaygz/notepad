ad18bdd3a1cbec52d416a1b56d50dfaea0362d3d
Luay Gharzeddine <luaygharzeddine@gmail.com>
2022-11-03 14:03:38 +0200

    commit

--- main.py
+++ main.py
@@ -6,51 +6,53 @@ from tkinter import Tk
 from ttkthemes import ThemedTk
 from tkinter.font import Font
 
-from utils import do_popup, close_popup, select_all, delete_selection, paste
+from utils import do_popup, close_popup, select_all, delete_selection, paste, get_current_word, get_rhymes, control_backspace
 
 class ScrollableText(Text):
-    def __init__(self, master=None, **kwargs):
+    def __init__(self, master=None, x_scroll=True, y_scroll=True, wrap=tk.NONE, **kwargs):
         self.frame = ttk.Frame(master)
-        self.x_scroll_bar = ttk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
-        self.x_scroll_bar.pack(side=tk.BOTTOM, fill=tk.X)
-        self.y_scroll_bar = ttk.Scrollbar(self.frame)
-        self.y_scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        Text.__init__(self, self.frame, wrap=tk.NONE,
-                      xscrollcommand=self.x_scroll_bar.set,
-                      yscrollcommand=self.y_scroll_bar.set,
+        if x_scroll:
+            self.x_scroll_bar = ttk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
+            self.x_scroll_bar.pack(side=tk.BOTTOM, fill=tk.X)
+            self.xscrollcommand = self.x_scroll_bar.set
+            self.x_scroll_bar.config(command=self.xview)
+        else:
+            self.xscrollcommand = None
+        if y_scroll:
+            self.y_scroll_bar = ttk.Scrollbar(self.frame)
+            self.y_scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+            self.yscrollcommand = self.y_scroll_bar.set
+            self.y_scroll_bar.config(command=self.yview)
+        else:
+            self.yscrollcommand = None
+        Text.__init__(self, self.frame, wrap=wrap,
+                      xscrollcommand=self.xscrollcommand,
+                      yscrollcommand=self.yscrollcommand,
+                      undo=True,
+                      autoseparators=True,
+                      maxundo=-1,
                       **kwargs)
+
         self.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-        self.x_scroll_bar.config(command=self.xview)
-        self.y_scroll_bar.config(command=self.yview)
+        # Select all
+        self.bind("<Control-Key-a>", lambda event: select_all(self))
+        self.bind("<Control-Key-A>", lambda event: select_all(self)) # In case caps lock
 
-        self.tag_configure("BOLD", font=Font(family="Helvetica", size=14, weight="bold"))
+        # Custom undo/redo behavior, undo deletes until previous word
+        self.bind("<space>", lambda event: self.edit_separator())
+        self.bind("<Return>", lambda event: self.edit_separator())
 
-    def highlight_pattern(self, pattern, tag, start="1.0", end="end",
-                          regexp=False):
-        '''Apply the given tag to all text that matches the given pattern
+        # By default ctrl+v doesn't overwrite selected text
+        self.bind("<<Paste>>", lambda event: delete_selection(self))
 
-        If 'regexp' is set to True, pattern will be treated as a regular
-        expression according to Tcl's regular expression syntax.
-        '''
+        # Control+backspace deletes entire word
+        self.bind("<Control-BackSpace>", lambda event: control_backspace(self))
 
-        start = self.index(start)
-        end = self.index(end)
-        self.mark_set("matchStart", start)
-        self.mark_set("matchEnd", start)
-        self.mark_set("searchLimit", end)
-
-        count = tk.IntVar()
-        while True:
-            index = self.search(pattern, "matchEnd", "searchLimit",
-                                count=count, regexp=regexp)
-            if index == "": break
-            if count.get() == 0: break # degenerate pattern which matches zero-length strings
-            self.mark_set("matchStart", index)
-            self.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
-            self.tag_add(tag, "matchStart", "matchEnd")
+        # Right click popup menu
+        self.popup_menu = PopupMenu(self)
+        self.bind("<Button-3>", lambda event: do_popup(event.x_root, event.y_root, self.popup_menu)) # Right click
+        self.popup_menu.bind("<FocusOut>", lambda event: close_popup(self.popup_menu))
 
 class StatusBar(Label):
     def __init__(self, master=None, **kwargs):
@@ -68,7 +70,7 @@ class PopupMenu(Menu):
         self.add_command(label="Cut", command=lambda: master.event_generate("<<Cut>>"))
         self.add_command(label="Copy", command=lambda: master.event_generate("<<Copy>>"))
         self.add_command(label="Paste", command=lambda: paste(master))
-        self.add_command(label="Save")
+        self.add_command(label="Explore Word")
 
 class Notepad(Tk):
     def __init__(self, **kwargs):
@@ -80,9 +82,6 @@ class Notepad(Tk):
         screen_height = self.winfo_screenheight()
         self.geometry(f"{screen_width}x{screen_height}")
         
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-
         self.menuBar = tk.Menu(self)
         self.fileMenu = tk.Menu(self.menuBar, tearoff=0)
         self.editMenu = tk.Menu(self.menuBar, tearoff=0)
@@ -90,63 +89,80 @@ class Notepad(Tk):
         self.helpMenu = tk.Menu(self.menuBar, tearoff=0)
         self.config(menu=self.menuBar)
 
-        # Main text area
-        self.text = ScrollableText(self, undo=True, autoseparators=True, maxundo=-1)
-        self.text.frame.grid(row=0, column=1, sticky="NSEW")
-
-        # Line numbers
-        self.line_numbers = tk.Canvas(self, width="30", height="500")
-        self.line_numbers.grid(row=0, column=0, sticky='NS', pady=1, rowspan=3)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
         # Status bar on the bottom
         self.status_bar = StatusBar(self)
         self.status_bar.grid(row=2, column=1, columnspan=2, sticky="EW")
 
-        # Right-click popup menu
-        self.popup_menu = PopupMenu(self.text)
+        # Line numbers on the left
+        self.line_numbers = tk.Canvas(self, width="30")
+        self.line_numbers.grid(row=0, column=0, sticky='NS', pady=1, rowspan=3)
 
-        # Custom undo/redo behavior, undo deletes until previous word
-        self.text.bind("<space>", lambda event: self.text.edit_separator())
-        self.text.bind("<Return>", lambda event: self.text.edit_separator())
+        # Main text area
+        self.editor = ScrollableText(self)
+        self.editor.frame.grid(row=0, column=1, sticky="NSEW", rowspan=2)
 
-        # Update word count
-        self.text.bind("<KeyRelease>", self.redraw)
+        # Info frame
+        self.info_frame = ttk.Frame(self, width="400")
+        self.info_frame.grid(row=0, column=2, sticky="NSEW")
 
-        # By default ctrl+v doesn't overwrite selected text
-        self.text.bind("<<Paste>>", lambda event: delete_selection(self.text))
+        # Suggested rhymes text area
+        self.suggested_rhymes = ScrollableText(self.info_frame, x_scroll=False, wrap=tk.WORD, state="disabled")
+        self.suggested_rhymes.frame.pack(side=tk.LEFT, anchor="n")
+        self.current_word = ""
 
-        # Select all
-        self.text.bind("<Control-Key-a>", lambda event: select_all(self.text))
-        self.text.bind("<Control-Key-A>", lambda event: select_all(self.text)) # In case caps lock
 
-        # Create and close right click popup
-        self.text.bind("<Button-3>", lambda event: do_popup(event.x_root, event.y_root, popup_menu)) # Right click
-        self.popup_menu.bind("<FocusOut>", lambda event: close_popup(popup_menu))
+        self.editor.bind("<KeyRelease>", self.update_rhymes)
+        self.editor.bind("<ButtonRelease>", self.update_rhymes) # Mouse
 
-        self.text.focus()
+        # Redraw status bar and line numbers
+        self.bind("<KeyRelease>", self.redraw)
+        self.bind("<Configure>", self.redraw) # Resizing window
+        self.bind('<Button-4>', self.redraw) # Scroll up
+        self.bind('<Button-5>', self.redraw) # Scroll down
+        self.bind('<Motion>', self.redraw) # Mouse move
+
+        self.editor.focus()
         self.redraw()
 
     def redraw(self, event=None):
         # Update status bar
-        words = self.text.get('1.0', tk.END).split()
+        words = self.editor.get('1.0', tk.END).split()
         word_count = len(words)
         self.status_bar.update(word_count)
 
         # Draw line numbers
         self.line_numbers.delete("all")
-        index = "1.0"
+        index = self.editor.index("@0,0") # First visible row
         while True:
-            line_info = self.text.dlineinfo(index)
+            line_info = self.editor.dlineinfo(index)
             if line_info is None:
                 break
             x_coord = 2
             y_coord = line_info[1]
             line_num = str(index).split(".")[0]
             self.line_numbers.create_text(x_coord, y_coord, anchor="nw", text=line_num, fill="black")
-            index = self.text.index(f"{index}+1line")
+            index = self.editor.index(f"{index}+1line")
 
-        # Highlight
-        # self.text.highlight_pattern("a", "BOLD")
+    def set_suggested_rhymes_text(self, text):
+        self.suggested_rhymes.configure(state="normal")
+        self.suggested_rhymes.delete("1.0", tk.END)
+        self.suggested_rhymes.insert("1.0", text)
+        self.suggested_rhymes.configure(state="disabled")
+
+    def update_rhymes(self, event=None):
+        lines = self.editor.get("1.0", tk.END).split("\n")
+        index = self.editor.index(tk.INSERT)
+        row = int(index.split(".")[0])
+        col = int(index.split(".")[1]) - 1 # Cursor points to char before it
+        line = lines[row-1]
+        current_word = get_current_word(line, col)
+        # print(self.current_word, current_word)
+        if self.current_word != current_word:
+            self.current_word = current_word
+            self.set_suggested_rhymes_text(get_rhymes(current_word))
 
 notepad = Notepad()
 notepad.mainloop()
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import tkinter as tk
-import pyperclip
+from Phyme import Phyme
+from Phyme.rhymeUtils import count_syllables, get_phones, extract_syllables
+
+phyme = Phyme()
 
 def do_popup(x, y, popup_menu):
     popup_menu.post(x, y)
@@ -23,3 +26,69 @@ def delete_selection(text_area):
 def paste(text_area):
     delete_selection(text_area)
     text_area.insert("insert", text_area.clipboard_get())
+
+def control_backspace(text_area):
+    cursor_index = text_area.index(tk.INSERT)
+    row, column = cursor_index.split(".")
+    column = int(column)
+    current_line = text_area.get("insert linestart", "insert lineend")
+    nearest_space = current_line.rfind(" ", 0, column)
+    if nearest_space == -1:
+        nearest_space = 0
+    start_index = row + "." + str(nearest_space)
+    text_area.tag_add(tk.SEL, start_index, cursor_index)
+
+def is_word_char(char):
+    return char.lower() in "abcdefghijklmnopqrstuvwxyz'"
+
+def get_current_word(text, index):
+    # If char is a nonword (like a space), but is at the boundary of a word on its right,
+    # move index to the right
+    # e.g.
+    # abc |def geh
+    #     ^ if the cursor is here, it gives the index of the space before it, but we want
+    #       to still assume we are referencing the next word
+    should_check_right = index < 0 or not is_word_char(text[index])
+    char_to_right_is_word = index < len(text) - 1 and is_word_char(text[index+1])
+    if should_check_right and char_to_right_is_word:
+        index += 1
+    if index < 0:
+        return ""
+    # Get left boundary
+    left_index = index
+    while left_index >= 0 and is_word_char(text[left_index]):
+        left_index -= 1
+    left_index += 1
+    # Get right boundary
+    right_index = index
+    while right_index < len(text) and is_word_char(text[right_index]):
+        right_index += 1
+    right_index -= 1
+    return text[left_index:right_index+1]
+
+def flatten_rhymes_dict(d, num_syllables):
+    rhymes = []
+    if num_syllables in d:
+        rhymes.extend(d[num_syllables])
+    for key, value in sorted(d.items(), reverse=True):
+        if key == num_syllables:
+            continue
+        rhymes.extend(value)
+    return rhymes
+
+def get_rhymes(word):
+    rhymes = []
+    try:
+        num_syllables = count_syllables(word)
+        # for i in range(num_syllables, 0, -1):
+        #     perfect_rhymes = phyme.get_perfect_rhymes(word, num_syllables=i)
+        #     rhymes.extend(flatten_rhymes_dict(perfect_rhymes, num_syllables))
+        for i in range(num_syllables, 0, -1):
+            assonance_rhymes = phyme.get_assonance_rhymes(word, num_syllables=i)
+            rhymes.extend(flatten_rhymes_dict(assonance_rhymes, num_syllables))
+    except:
+        pass
+    rhymes = list(dict.fromkeys(rhymes)) # Get unique
+    rhymes = rhymes[:1000]
+    rhymes = [w for w in rhymes if w != word]
+    return rhymes
